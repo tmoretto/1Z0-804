7050397347c74c117c0f4c09e609b4f881ab8d41
Tiago Moretto <moretto.tiago@gmail.com>
2018-01-26 19:30:22 -0200

    Class Design Labs

---
+++ src/br/com/cert/labs/classdesign/StaticOrderTest.java
@@ -0,0 +1,16 @@
+package br.com.cert.labs.classdesign;
+
+public class StaticOrderTest {
+
+	private static int x = getValue();
+	private static int y = 5;
+
+	private static int getValue() {
+		return y;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(x);
+	}
+
+}
