22133d335bb14465b7a262ec3846d9a32ca68884
Tiago Moretto <moretto.tiago@gmail.com>
2018-01-27 17:18:04 -0200

    Forward Reference Test

---
+++ src/br/com/cert/labs/classdesign/ForwardReferenceTest.java
@@ -0,0 +1,24 @@
+package br.com.cert.labs.classdesign;
+
+public class ForwardReferenceTest {
+
+	public ForwardReferenceTest() {
+		super();
+	}
+
+	public ForwardReferenceTest echoReference() {
+		return this;
+	}
+
+	public void testLegalForwardReference() {
+		// Illustration: Legal
+		this.x = 5;
+	}
+
+	private int x = 0;
+
+	// Illustration: Illegal
+	private ForwardReferenceTest b = a.echoReference();
+	private ForwardReferenceTest a = new ForwardReferenceTest();
+
+}
--- src/br/com/cert/labs/classdesign/StaticOrderTest.java
+++ src/br/com/cert/labs/classdesign/StaticOrderTest.java
@@ -4,13 +4,15 @@ public class StaticOrderTest {
 
 	private static int x = getValue();
 	private static int y = 5;
+	private static int z = getValue();
 
 	private static int getValue() {
 		return y;
 	}
 
 	public static void main(String[] args) {
-		System.out.println(x); //prints zero
+		System.out.println(x); //prints 0
+		System.out.println(z); //prints 5
 	}
 
 }
