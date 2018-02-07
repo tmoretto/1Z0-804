e1a8b27a16ee47c1e5450742519078c86d1af41a
Tiago Moretto <moretto.tiago@gmail.com>
2018-02-07 15:16:13 -0200

    More labs

---
+++ src/br/com/cert/labs/io/FileHierarchical.java
@@ -0,0 +1,9 @@
+package br.com.cert.labs.io;
+
+public class FileHierarchical {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/br/com/cert/labs/io/ScannerTest.java
@@ -0,0 +1,23 @@
+package br.com.cert.labs.io;
+
+import java.util.Scanner;
+
+public class ScannerTest {
+
+	public static void main(String[] args) {
+		String input = "1 fish 2 fish red fish blue fish";
+
+		// s* means 0 or more repetitions of any whitespace character
+		// fish is the pattern to find
+		Scanner s = new Scanner(input).useDelimiter("\\s*fish\\s*");
+		
+		System.out.println(s.nextInt()); // prints: 1
+		System.out.println(s.nextInt()); // prints: 2
+		System.out.println(s.next()); // prints: red
+		System.out.println(s.next()); // prints: blue
+
+		// don't forget to close the scanner!!
+		s.close();
+	}
+
+}
