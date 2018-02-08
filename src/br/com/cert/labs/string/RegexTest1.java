53f677c4dea014791d5e09d1a51a9bce376ecece
Tiago Moretto <moretto.tiago@gmail.com>
2018-02-08 17:07:44 -0200

    More labs

--- src/br/com/cert/labs/string/RegexTest1.java
+++ src/br/com/cert/labs/string/RegexTest1.java
@@ -73,9 +73,68 @@ public class RegexTest1 {
 		    // index in the input string that the group was found.
 		    System.out.println(String.format("Month found at[%d, %d])", 
 		        matcher.start(1), matcher.end(1)));
+
+		    System.out.println(String.format("Day found at[%d, %d])", 
+			        matcher.start(2), matcher.end(2)));
 		}
 		
 		assertNotNull(matcher);
 		
 	}
+	
+	@Test
+	public void testForReal() {
+		
+		String regex = "([0-9A-Za-z]+)";
+		
+		System.out.println(Pattern.matches(regex, "JAMES louis PATTINSON"));
+		
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher("JAMES1 louis2 PATTINSON3");
+		
+		while (matcher.find()) {
+			System.out.println(String.format("Match: %s index i: %d index f: %d", matcher.group(), matcher.start(), matcher.end()));
+		}
+		
+		assertNotNull(matcher);
+		
+	}
+	
+	@Test
+	public void testForReal1() {
+		//What will the following code snippet print when run using the 
+		//command line arguments: \s[\d]*\s   "12 3abc ab23"  (There are only single spaces in the second argument.) 
+	
+		Pattern p = Pattern.compile("\\s[\\d]*\\s");    
+		Matcher m = p.matcher("12 3abc ab23");
+		boolean b = false;    
+		while(b = m.find()) {        
+			System.out.println(m.start()+" "+m.group());    
+		} 
+				
+		assertNotNull(m);
+	}
+	
+	@Test
+	public void testReplace() {
+		// Lets try and reverse the order of the day and month in a few date 
+		// strings. Notice how the replacement string also contains metacharacters
+		// (the back references to the captured groups) so we use a verbatim 
+		// string for that as well.
+		Pattern ptrn = Pattern.compile("([a-zA-Z]+) (\\d+)");
+		Matcher matcher = ptrn.matcher("June 24, August 9, Dec 12");
+
+		// This will reorder the string inline and print:
+		//   24 of June, 9 of August, 12 of Dec
+		// Remember that the first group is always the full matched text, so the 
+		// month and day indices start from 1 instead of zero.
+		String replacedString = matcher.replaceAll("$2 of $1");
+		System.out.println(replacedString);
+		
+		String replacedStringFirst = matcher.replaceFirst("$2 of $1");
+		System.out.println(replacedStringFirst);
+
+		assertNotNull(matcher);
+	}
+
 }
