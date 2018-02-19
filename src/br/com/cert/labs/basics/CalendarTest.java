ff71b913c7525a1a64d2f750205059e2a9a33b46
Tiago Moretto <moretto.tiago@gmail.com>
2018-02-19 06:52:22 -0300

    More labs

---
+++ src/br/com/cert/labs/basics/CalendarTest.java
@@ -0,0 +1,22 @@
+package br.com.cert.labs.basics;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class CalendarTest {
+
+	public static void main(String[] args) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(new Date());
+		
+		cal.roll(Calendar.MONTH, 11);
+		
+		System.out.println(cal.getTime());
+		
+		cal.set(Calendar.MONTH, Calendar.FEBRUARY);
+		
+		System.out.println(cal.getTime());
+
+	}
+
+}
